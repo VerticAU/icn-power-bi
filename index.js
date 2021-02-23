7c6161c0707a6923fb6667c2b7b5569cc6505698
Vlad Khitrik <vlad.khitrik@vertic.by>
2021-02-23 14:56:33 +0300

    Get Encrypted Data Test

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var Cipher = require('aes-ecb');
+
 var app = express();
 
 app
@@ -12,8 +14,21 @@ app
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 // GET method route
-app.get('/test', function (req, res) {
-  res.send('GET request to the homepage');
+app.get('/get-encrypted-data', function (req, res) {
+  var cardNo = '1234567890123456';
+  var expYear = '25';
+  var expMonth = '12';
+  var idNo = '800212';
+  var cardPw = '12';
+  var merchantKey = 'TPP4afX4e5US6FEl0MnoyRHT/yzTRZVrKGJVBmew66y8jSDOt5ZNigM0DM/WZdYbev7OV/lTUEewzhq5dqKygg==';
+
+  var keyString = merchantKey.substr(0, 16);
+
+  var plainText = `CardNo=${cardNo}&ExpYear=${expYear}&ExpMonth=${expMonth}&IDNo=${idNo}&CardPw=${cardPw}`;
+
+  var encrypt = Cipher.encrypt(keyString, plainText);
+
+  res.send(encrypt);
 });
 
 // POST method route
